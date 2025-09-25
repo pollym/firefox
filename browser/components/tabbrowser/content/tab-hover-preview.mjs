e7b2da7d5a80afcb579aacd4456695dc8ac181dc
Jeremy Swinarton <jswinarton@mozilla.com>
2025-09-25 21:32:24 +0000

    Bug 1980036: Make the THP/TGHP panel respect changes to ui.tooltip.delay_ms r=dwalker,tabbrowser-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D265982

--- browser/components/tabbrowser/content/tab-hover-preview.mjs
+++ browser/components/tabbrowser/content/tab-hover-preview.mjs
@@ -42,16 +42,10 @@ export default class TabHoverPanelSet {
       "ui.popup.disable_autohide",
       false
     );
-    XPCOMUtils.defineLazyPreferenceGetter(
-      this,
-      "_prefPreviewDelay",
-      "ui.tooltip.delay_ms"
-    );
 
     this.#win = win;
 
     this.panelOpener = new TabPreviewPanelTimedFunction(
-      this._prefPreviewDelay,
       ZERO_DELAY_ACTIVATION_TIME,
       this.#win
     );
@@ -639,9 +633,6 @@ class TabGroupPanel extends Panel {
  */
 class TabPreviewPanelTimedFunction {
   /** @type {number} */
-  #delay;
-
-  /** @type {number} */
   #zeroDelayTime;
 
   /** @type {Window} */
@@ -653,8 +644,13 @@ class TabPreviewPanelTimedFunction {
   /** @type {boolean} */
   #useZeroDelay;
 
-  constructor(delay, zeroDelayTime, win) {
-    this.#delay = delay;
+  constructor(zeroDelayTime, win) {
+    XPCOMUtils.defineLazyPreferenceGetter(
+      this,
+      "_prefPreviewDelay",
+      "ui.tooltip.delay_ms"
+    );
+
     this.#zeroDelayTime = zeroDelayTime;
     this.#win = win;
 
@@ -675,7 +671,7 @@ class TabPreviewPanelTimedFunction {
         this.#timer = null;
         target();
       },
-      this.#useZeroDelay ? 0 : this.#delay
+      this.#useZeroDelay ? 0 : this._prefPreviewDelay
     );
   }
 
