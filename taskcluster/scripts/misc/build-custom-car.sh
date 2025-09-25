f76341b1c6a5866a4103d499b48867b73f40ec3a
KS <kshampur@mozilla.com>
2025-09-25 23:30:38 +0000

    Bug 1989676 - Fix MacOS CaR builds by updating SDK. r=mac-reviewers,mstange
    This patch adds the MacOSX26.0.sdk which is now required for building
     MacOS CaR (Chromium-as-Release) builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266258

--- taskcluster/scripts/misc/build-custom-car.sh
+++ taskcluster/scripts/misc/build-custom-car.sh
@@ -60,7 +60,14 @@ fi
 # Logic for macosx64
 if [[ $(uname -s) == "Darwin" ]]; then
   # Modify the config with fetched sdk path
-  export MACOS_SYSROOT="$MOZ_FETCHES_DIR/MacOSX15.4.sdk"
+  export MACOS_SYSROOT="$MOZ_FETCHES_DIR/MacOSX26.0.sdk"
+  # Bug 1990712 & 1989676
+  # HACK: Create a stub DarwinBasic.modulemap to satisfy Ninja’s dependency graph.
+  # This file does not exist in Command Line Tools SDKs. It seems only the full
+  # Xcode SDK includes DarwinBasic/DarwinFoundation modulemaps.
+  mkdir -p "$MACOS_SYSROOT/usr/include"
+  touch "$MACOS_SYSROOT/usr/include/DarwinFoundation.modulemap"
+
 
   # Avoid mixing up the system python and toolchain python in the
   # python path configuration
