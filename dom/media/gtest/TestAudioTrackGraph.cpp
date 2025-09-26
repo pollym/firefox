5a0176dd0c23dc6ff702edbc7c4db5c9361a306e
Joel Maher <joel.maher@gmail.com>
2025-09-26 19:49:02 +0000

    Bug 1987870 - Support GTEST_SKIP for gtest harness. r=ci-and-tooling,aryx
    Differential Revision: https://phabricator.services.mozilla.com/D266088

--- dom/media/gtest/TestAudioTrackGraph.cpp
+++ dom/media/gtest/TestAudioTrackGraph.cpp
@@ -3400,16 +3400,13 @@ TEST(TestAudioTrackGraph, EmptyProcessingInterval)
   (void)WaitFor(destroyPromise).unwrap()[0];
 }
 
-#ifndef ANDROID  // Workaround for bug 1987870 making GTEST_SKIP() ineffective.
 TEST(TestAudioTrackGraph, DefaultOutputDeviceIDTracking)
 {
-#  if 0  // Bug 1987870, see above.
-#    ifdef ANDROID
+#ifdef ANDROID
   GTEST_SKIP() << "On Android CubebDeviceEnumerator, not the cubeb backend, "
                   "handles device enumeration, exposing only a single input "
                   "and output device. Both with devid 0.";
-#    endif
-#  endif
+#else
   MockCubeb* cubeb = new MockCubeb(MockCubeb::RunningMode::Manual);
   AddDevices(cubeb, 1, CUBEB_DEVICE_TYPE_INPUT);
   AddDevices(cubeb, 2, CUBEB_DEVICE_TYPE_OUTPUT);
@@ -3515,8 +3512,8 @@ TEST(TestAudioTrackGraph, DefaultOutputDeviceIDTracking)
   // reference, and before the next test might ForceSetCubebContext() to
   // destroy our cubeb.
   (void)WaitFor(destroyPromise).unwrap()[0];
-}
 #endif
+}
 
 #undef Invoke
 #undef DispatchFunction
