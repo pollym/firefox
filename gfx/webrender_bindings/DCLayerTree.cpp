b5ffe7334ed4921565c542d8759db3f5fb43e9a0
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2025-09-27 07:32:19 +0000

    Bug 1990405 - Add video overlay support of P010 video r=media-playback-reviewers,alwu
    Fix P010 format handling with zero copy video.
    
    Differential Revision: https://phabricator.services.mozilla.com/D265934

--- gfx/webrender_bindings/DCLayerTree.cpp
+++ gfx/webrender_bindings/DCLayerTree.cpp
@@ -1976,7 +1976,8 @@ void DCSurfaceVideo::AttachExternalImage(wr::ExternalImageId aExternalImage) {
   // XXX if software decoded video frame format is nv12, it could be used as
   // video overlay.
   if (!texture || !texture->AsRenderDXGITextureHost() ||
-      texture->GetFormat() != gfx::SurfaceFormat::NV12) {
+      ((texture->GetFormat() != gfx::SurfaceFormat::NV12) &&
+       (texture->GetFormat() != gfx::SurfaceFormat::P010))) {
     gfxCriticalNote << "Unsupported RenderTexture for overlay: "
                     << gfx::hexa(texture);
     return;
