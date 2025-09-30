aad3d55d1816bc2d71ba36b7a6904ec511516596
Titouan Thibaud <tthibaud@mozilla.com>
2025-09-30 09:43:43 +0000

    Bug 1991434 - Use Android Components animateRotation on Translation's DownloadIndicator. r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D266675

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/DownloadIndicator.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/DownloadIndicator.kt
@@ -4,12 +4,6 @@
 
 package org.mozilla.fenix.translations
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,10 +15,8 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.painter.Painter
@@ -37,6 +29,7 @@ import androidx.compose.ui.semantics.role
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import mozilla.components.compose.base.button.FilledButton
+import mozilla.components.compose.base.modifier.animateRotation
 import org.mozilla.fenix.R
 import org.mozilla.fenix.theme.FirefoxTheme
 import mozilla.components.ui.icons.R as iconsR
@@ -66,7 +59,7 @@ fun DownloadIconIndicator(
         painter = icon,
         modifier = modifier.then(
             Modifier
-                .rotate(rotationAnimation()),
+                .animateRotation(animate = true, durationMillis = ANIMATION_DURATION_MS),
         ),
         contentDescription = contentDescription,
         tint = tint,
@@ -135,24 +128,10 @@ fun DownloadIndicator(
         ),
         icon = icon,
         iconModifier = Modifier
-            .rotate(rotationAnimation())
+            .animateRotation(animate = true, durationMillis = ANIMATION_DURATION_MS)
             .size(ButtonDefaults.IconSize),
         onClick = {},
     )
-}
-
-@Composable
-internal fun rotationAnimation(): Float {
-    val infiniteTransition = rememberInfiniteTransition()
-    val angle by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(ANIMATION_DURATION_MS, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart,
-        ),
-    )
-    return angle
 }
 
 @Composable
