5c868836c8f6914046dc19e8d74bb70ae535d46c
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2025-09-30 15:28:03 +0000

    Bug 1967204 - remove superfluous eslint-env annotations from browser cmponents and actors, r=frontend-codestyle-reviewers,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D266832

--- browser/components/tabbrowser/content/tabs.js
+++ browser/components/tabbrowser/content/tabs.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint-env mozilla/browser-window */
-
 "use strict";
 
 // This is loaded into all browser windows. Wrap in a block to prevent
