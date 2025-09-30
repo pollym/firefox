25abb43ee9a212b7df21e9279cd33a42ef452e4f
Randell Jesup <rjesup@mozilla.com>
2025-09-30 04:08:34 +0000

    Bug 1917965: Update cache listener changes to ensure data is cached in compressed form r=necko-reviewers,kershaw
    The changes to the listener to support saving to cache didn't
    guarantee that compressed data would be stored in the cache in compressed
    form.  Also the Content-Encoding values stored in the cache metadata could
    be incorrect, leading to subtle CI failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D264526

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -421,8 +421,15 @@ class nsHttpChannel final : public HttpBaseChannel,
                        bool aModified);
   [[nodiscard]] nsresult AddCacheEntryHeaders(nsICacheEntry* entry,
                                               bool aModified);
+  [[nodiscard]] nsresult ProcessVaryCacheEntryHeaders(nsICacheEntry* entry,
+                                                      const nsHttpAtom* aAtom);
+  [[nodiscard]] nsresult ModifiedCacheEntryHeaders(nsICacheEntry* entry,
+                                                   const nsHttpAtom& aAtom);
   [[nodiscard]] nsresult FinalizeCacheEntry();
   [[nodiscard]] nsresult InstallCacheListener(int64_t offset = 0);
+  [[nodiscard]] nsresult DoInstallCacheListener(bool aIsDictionaryCompressed,
+                                                nsACString* aDictionary,
+                                                int64_t offset = 0);
   void MaybeInvalidateCacheEntryForSubsequentGet();
   void AsyncOnExamineCachedResponse();
 
