3b08e71bb5bef3f2ad259f0ab86f93aef85cd149
Mike Conley <mconley@mozilla.com>
2025-10-01 18:39:30 +0000

    Bug 1991490 - Cancel in-process XPIInstalls on quit-application-granted. r=rpl
    Normally, the network:offline-about-to-go-offline topic would satisfy here - however,
    in cases where we have an AsyncShutdown blocker that's preventing us from reaching
    network:offline-about-to-go-offline AND that blocker is awaiting on an XPIInstall,
    the quit-application-granted topic can help break that circular dependency.
    
    Differential Revision: https://phabricator.services.mozilla.com/D267029

--- toolkit/mozapps/extensions/internal/XPIInstall.sys.mjs
+++ toolkit/mozapps/extensions/internal/XPIInstall.sys.mjs
@@ -99,6 +99,8 @@ const PREF_XPI_WEAK_SIGNATURES_ALLOWED =
 const PREF_SELECTED_THEME = "extensions.activeThemeID";
 
 const TOOLKIT_ID = "toolkit@mozilla.org";
+const TOPIC_GOING_OFFLINE = "network:offline-about-to-go-offline";
+const TOPIC_QUIT_GRANTED = "quit-application-granted";
 
 ChromeUtils.defineLazyGetter(lazy, "MOZ_UNSIGNED_SCOPES", () => {
   let result = 0;
@@ -2424,9 +2426,11 @@ var DownloadAddonInstall = class extends AddonInstall {
     }
   }
 
-  observe() {
-    // Network is going offline
-    this.cancel();
+  observe(_subject, topic, _data) {
+    if (topic == TOPIC_GOING_OFFLINE || topic == TOPIC_QUIT_GRANTED) {
+      // Network is going offline, or we're shutting down
+      this.cancel();
+    }
   }
 
   /**
@@ -2515,7 +2519,8 @@ var DownloadAddonInstall = class extends AddonInstall {
       }
       this.channel.asyncOpen(listener);
 
-      Services.obs.addObserver(this, "network:offline-about-to-go-offline");
+      Services.obs.addObserver(this, TOPIC_GOING_OFFLINE);
+      Services.obs.addObserver(this, TOPIC_QUIT_GRANTED);
     } catch (e) {
       logger.warn(
         "Failed to start download for addon " + this.sourceURI.spec,
@@ -2632,7 +2637,8 @@ var DownloadAddonInstall = class extends AddonInstall {
     this.stream.close();
     this.channel = null;
     this.badCerthandler = null;
-    Services.obs.removeObserver(this, "network:offline-about-to-go-offline");
+    Services.obs.removeObserver(this, TOPIC_GOING_OFFLINE);
+    Services.obs.removeObserver(this, TOPIC_QUIT_GRANTED);
 
     let crypto = this.crypto;
     this.crypto = null;
