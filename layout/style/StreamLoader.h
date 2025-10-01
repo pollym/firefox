52296e3525e6eb23533e60cd69df3a0542c51bdf
smayya <smayya@mozilla.com>
2025-10-01 07:57:08 +0000

    Bug 1989184 -  remove metrics  networking.http_content_cssloader_ondatafinished_to_onstop_delay. r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D266799

--- layout/style/StreamLoader.h
+++ layout/style/StreamLoader.h
@@ -64,8 +64,6 @@ class StreamLoader : public nsIThreadRetargetableStreamListener,
   bool mOnStopProcessingDone{false};
   RefPtr<SheetLoadDataHolder> mMainThreadSheetLoadData;
 
-  mozilla::TimeStamp mOnDataFinishedTime;
-
 #ifdef NIGHTLY_BUILD
   bool mChannelOpenFailed = false;
 #endif
