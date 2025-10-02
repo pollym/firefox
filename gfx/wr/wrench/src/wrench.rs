d756df7b53c1fdccc88c1d2cce7f2c1a83e14a32
Nicolas Silva <nical@fastmail.com>
2025-10-02 06:59:27 +0000

    Bug 1978773 - Enable precise radial gradients (GPU-only). r=gfx-reviewers,lsalzman
    A followup patch will enable this for SWGL.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266816

--- gfx/wr/wrench/src/wrench.rs
+++ gfx/wr/wrench/src/wrench.rs
@@ -280,6 +280,7 @@ impl Wrench {
             clear_caches_with_quads: !window.is_software(),
             compositor_config,
             enable_debugger: true,
+            precise_radial_gradients: !window.is_software(),
             ..Default::default()
         };
 
