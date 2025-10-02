528ad9eaef52adc0560257509b8d887736a335e1
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-10-02 23:57:39 +0000

    Bug 1990550 - Add new profiles callout when first profile created. r=profiles-reviewers,omc-reviewers,jprickett,jhirsch,fluent-reviewers,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D266227

--- browser/components/profiles/tests/browser/browser_appmenu_menuitem_updates.js
+++ browser/components/profiles/tests/browser/browser_appmenu_menuitem_updates.js
@@ -35,6 +35,19 @@ add_task(async function test_appmenu_updates_on_edit() {
 
   SelectableProfileService.currentProfile.name = INITIAL_NAME;
   await promiseAppMenuOpened();
+
+  // Close the instructional callout, then resume the test.
+  await TestUtils.waitForCondition(() =>
+    document.querySelector("button.dismiss-button")
+  );
+  document.querySelector("button.dismiss-button").click();
+  await TestUtils.waitForCondition(
+    () => !document.querySelector("button.dismiss-button")
+  );
+  // The callout leaves some residue on the window which breaks the test. TODO fix.
+  delete window.React;
+  delete window.ReactDOM;
+
   let view = PanelMultiView.getViewNode(document, "appMenu-profiles-button");
   Assert.equal(view.label, INITIAL_NAME, "expected the initial name");
 
