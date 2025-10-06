46dda4f4969e1e4bd1a32308b5fd6ec04e9db4e7
Nick Grato <ngrato@gmail.com>
2025-10-06 23:24:58 +0000

    Bug 1990881 - basic intent ask vs search vs navigate heuristic r=Mardak,firefox-ai-ml-reviewers
    building on bug 1989035 doing chat, sometimes we want it to search instead. we can do some very basic heuristic to set up an async api for it to be likely replaced by a engine/model
    
    to start, this could reflect text of the submit button based on the typed text. eventually this might be reused for classifying quick prompts and suggestions
    
    Differential Revision: https://phabricator.services.mozilla.com/D267500

--- browser/components/genai/content/smart-assist.mjs
+++ browser/components/genai/content/smart-assist.mjs
@@ -29,6 +29,7 @@ export class SmartAssist extends MozLitElement {
     mode: { type: String }, // "tab" | "sidebar"
     overrideNewTab: { type: Boolean },
     showLog: { type: Boolean },
+    actionKey: { type: String }, // "chat" | "search"
   };
 
   constructor() {
@@ -45,6 +46,20 @@ export class SmartAssist extends MozLitElement {
     this.overrideNewTab = Services.prefs.getBoolPref(
       "browser.ml.smartAssist.overrideNewTab"
     );
+    this.actionKey = "chat";
+
+    this._actions = {
+      chat: {
+        label: "Submit",
+        icon: "chrome://global/skin/icons/arrow-right.svg",
+        run: this._actionChat,
+      },
+      search: {
+        label: "Search",
+        icon: "chrome://global/skin/icons/search-glass.svg",
+        run: this._actionSearch,
+      },
+    };
   }
 
   connectedCallback() {
@@ -70,12 +85,27 @@ export class SmartAssist extends MozLitElement {
     this.logState = [...this.logState, entryWithDate];
   };
 
-  _handlePromptInput = e => {
+  _handlePromptInput = async e => {
     const value = e.target.value;
     this.userPrompt = value;
+
+    // Determine intent based on keywords in the prompt
+    this.actionKey = await lazy.SmartAssistEngine.getPromptIntent(value);
   };
 
-  _handleSubmit = async () => {
+  /**
+   * Returns the current action object based on the actionKey
+   */
+
+  get inputAction() {
+    return this._actions[this.actionKey];
+  }
+
+  _actionSearch = () => {
+    // TODO: Implement search functionality
+  };
+
+  _actionChat = async () => {
     const formattedPrompt = (this.userPrompt || "").trim();
     if (!formattedPrompt) {
       return;
@@ -126,7 +156,7 @@ export class SmartAssist extends MozLitElement {
    * Mock Functionality to open full page UX
    *
    * @param {boolean} enable
-   *   Whether or not to override the new tab page.
+   * Whether or not to override the new tab page.
    */
   _applyNewTabOverride(enable) {
     try {
@@ -223,12 +253,14 @@ export class SmartAssist extends MozLitElement {
             @input=${e => this._handlePromptInput(e)}
           ></textarea>
           <moz-button
+            iconSrc=${this.inputAction.icon}
             id="submit-user-prompt-btn"
             type="primary"
             size="small"
-            @click=${this._handleSubmit}
+            @click=${this.inputAction.run}
+            iconPosition="end"
           >
-            Submit
+            ${this.inputAction.label}
           </moz-button>
 
           <!-- Footer - New Tab Override -->
