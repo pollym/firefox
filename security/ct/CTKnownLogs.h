297b5e6f8c44aaf3c357e9b955e730566aae1cf1
ffxbld <ffxbld@mozilla.com>
2025-10-06 11:32:37 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D267577

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1765450866000000);
+static const PRTime kCTExpirationTime = INT64_C(1765796154000000);
 
 namespace mozilla::ct {
 
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -746,4 +746,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1767870036060000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1768215322806000);
