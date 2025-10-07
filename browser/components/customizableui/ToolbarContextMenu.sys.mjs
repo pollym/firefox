dbfb8f0b05b4a21cb1cc522f90a4610156a87879
agoloman <agoloman@mozilla.com>
2025-10-07 20:32:13 +0300

    Revert "Bug 1974265 - Add "Turn on Vertical Tabs" option to titlebar spacers context menu. r=nsharpley,dao" for causing bc failures @browser_customization_context_menus.js.
    This reverts commit ee12cad10d4b5229065feca05b204be1ef88cddd.

--- browser/components/customizableui/ToolbarContextMenu.sys.mjs
+++ browser/components/customizableui/ToolbarContextMenu.sys.mjs
@@ -232,9 +232,6 @@ export var ToolbarContextMenu = {
     let removeFromToolbar = popup.querySelector(
       ".customize-context-removeFromToolbar"
     );
-
-    let isTitlebarSpacer = toolbarItem?.classList.contains("titlebar-spacer");
-
     // Show/hide fullscreen context menu items and set the
     // autohide item's checked state to mirror the autohide pref.
     showFullScreenViewContextMenuItems(popup);
@@ -243,9 +240,7 @@ export var ToolbarContextMenu = {
     let sidebarRevampEnabled = Services.prefs.getBoolPref("sidebar.revamp");
     let showSidebarActions =
       ["tabbrowser-tabs", "sidebar-button"].includes(toolbarItem?.id) ||
-      toolbarItem?.localName == "toolbarspring" ||
-      isTitlebarSpacer;
-
+      toolbarItem?.localName == "toolbarspring";
     let toggleVerticalTabsItem = document.getElementById(
       "toolbar-context-toggle-vertical-tabs"
     );
@@ -266,8 +261,7 @@ export var ToolbarContextMenu = {
       !showSidebarActions || isVerticalTabStripMenu;
     document.getElementById("customizationMenuSeparator").hidden =
       toolbarItem?.id == "tabbrowser-tabs" ||
-      toolbarItem?.localName == "toolbarspring" ||
-      isTitlebarSpacer;
+      toolbarItem?.localName == "toolbarspring";
 
     // View -> Toolbars menu doesn't have the moveToPanel or removeFromToolbar items.
     if (!moveToPanel || !removeFromToolbar) {
