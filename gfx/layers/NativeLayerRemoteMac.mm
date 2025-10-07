f78cdbaff8a375d2b614c9434f2a59eeeabbb6ce
Markus Stange <mstange.moz@gmail.com>
2025-10-07 15:49:45 +0000

    Bug 1992515 - Use IOSurfacePort in CommandChangedSurface. r=bradwerth
    Differential Revision: https://phabricator.services.mozilla.com/D267489

--- gfx/layers/NativeLayerRemoteMac.mm
+++ gfx/layers/NativeLayerRemoteMac.mm
@@ -247,18 +247,15 @@ void NativeLayerRemoteMac::FlushDirtyLayerInfoToCommandQueue() {
   auto ID = reinterpret_cast<uint64_t>(this);
 
   if (mDirtyChangedSurface) {
-    uint32_t surfaceID = 0;
+    IOSurfacePort surfacePort;
     auto surfaceWithInvalidRegion = FrontSurface();
     if (surfaceWithInvalidRegion) {
-      // Get the unique ID for this IOSurfaceRef, which only works
-      // because kIOSurfaceIsGlobal was set to true when this
-      // IOSurface was created.
-      IOSurfaceRef surfaceRef = surfaceWithInvalidRegion->mSurface.get();
-      surfaceID = IOSurfaceGetID(surfaceRef);
+      surfacePort =
+          IOSurfacePort::FromSurface(surfaceWithInvalidRegion->mSurface);
     }
 
     mCommandQueue->AppendCommand(mozilla::layers::CommandChangedSurface(
-        ID, surfaceID, IsDRM(), IsHDR(), GetSize()));
+        ID, std::move(surfacePort), IsDRM(), IsHDR(), GetSize()));
     mDirtyChangedSurface = false;
   }
 
