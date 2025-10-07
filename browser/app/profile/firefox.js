bf831dda4234b9f9c65afdc05d1f6aba4461e8a4
Kelly Cochrane <kcochrane@mozilla.com>
2025-10-07 12:34:41 +0000

    Bug 1983125 - Make delay for the sidebar's expand on hover feature a customizable pref r=jsudiaman
    Differential Revision: https://phabricator.services.mozilla.com/D267677

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2171,6 +2171,7 @@ pref("sidebar.revamp.round-content-area", false);
 pref("sidebar.animation.enabled", true);
 pref("sidebar.animation.duration-ms", 200);
 pref("sidebar.animation.expand-on-hover.duration-ms", 400);
+pref("sidebar.animation.expand-on-hover.delay-duration-ms", 200);
 
 // This pref is used to store user customized tools in the sidebar launcher and shouldn't be changed.
 // See https://firefox-source-docs.mozilla.org/browser/components/sidebar/docs/index.html for ways
