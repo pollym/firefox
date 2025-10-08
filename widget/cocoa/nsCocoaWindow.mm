5fcd9d3b05fad811dc216aa807b6da985d28e541
Sam Johnson <sam@scj.me>
2025-10-08 08:51:35 +0000

    Bug 1993148 - Update popup border radius for macOS Tahoe. r=emilio,desktop-theme-reviewers
    Addressing a review comment from bug 1992894. Includes a minor refactor so that the corner radius comes from a single place.
    
    Differential Revision: https://phabricator.services.mozilla.com/D267899

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -7588,9 +7588,13 @@ static NSMutableSet* gSwizzledFrameViewClasses = nil;
   return self;
 }
 
+static CGFloat GetMenuCornerRadius() {
+  return nsCocoaFeatures::OnTahoeOrLater() ? 12.0f : 6.0f;
+}
+
 // Returns an autoreleased NSImage.
 static NSImage* GetMenuMaskImage() {
-  const CGFloat radius = 6.0f;
+  const CGFloat radius = GetMenuCornerRadius();
   const NSSize maskSize = {radius * 3.0f, radius * 3.0f};
   NSImage* maskImage = [NSImage imageWithSize:maskSize
                                       flipped:FALSE
@@ -7616,7 +7620,7 @@ static NSImage* GetMenuMaskImage() {
         // Menus on macOS 26 use glass instead of vibrancy.
         auto* effectView =
             [[NSGlassEffectView alloc] initWithFrame:self.contentView.frame];
-        effectView.cornerRadius = 12.0f;
+        effectView.cornerRadius = GetMenuCornerRadius();
         return effectView;
       }
     }
