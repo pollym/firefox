2619342232a6b718fe22d3dc365cad309efa61a2
Cristina Horotan <chorotan@mozilla.com>
2025-10-09 03:27:06 +0300

    Revert "Bug 1954427, Bug 1974686, Bug 1973976, Bug 1936429, 1898895, Bug 1873166, Bug 1993201 - add an Android-only UA override for eolno.nokas.com to get their mobile layout; r=webcompat-reviewers,ksenia" for causing bc failures on browser_parsable_css.js
    This reverts commit 4fe8c4e9ffe2ad25bd7278dcf4159aa3567cd6be.
    
    Revert "Bug 1974686 - add a CSS webcompat intervention for www.hermes.admin.ch; r=webcompat-reviewers,ksenia"
    
    This reverts commit 813b75b82462b89abc58dbc251c2b553fb8282d1.
    
    Revert "Bug 1973976 - add a CSS webcompat intervention for iweather.gov.vn; r=webcompat-reviewers,ksenia"
    
    This reverts commit 7d589adb41574ddc83eef54a982c53aff8c52175.
    
    Revert "Bug 1936429 - add a CSS intervention for diadora.com to hide extra scrollbars; r=webcompat-reviewers,ksenia"
    
    This reverts commit 0dd03dceb8a1fcc0f584acad0fa0348122af3c63.
    
    Revert "Bug 1898895 - add an Android-only UA override for eslatam.trygalaxy.com; r=webcompat-reviewers,ksenia"
    
    This reverts commit ea18f3782f55eb028c944684f0f0d439f4d52304.
    
    Revert "Bug 1873166 - update our webcompat intervention for nsandi.com; r=webcompat-reviewers,ksenia"
    
    This reverts commit 81a22c8a57e5df0ec64f525ccaa9775d57dc334b.
    
    Revert "Bug 1993201 - add a UA override for portal.pilot.ly; r=webcompat-reviewers,ksenia"
    
    This reverts commit 15b052cacc1b0547649881fa45b908fade8a38ed.

--- testing/webcompat/interventions/tests/test_1873166_108319_www_nsandi_com.py
+++ testing/webcompat/interventions/tests/test_1873166_108319_www_nsandi_com.py
@@ -3,20 +3,21 @@ import pytest
 URL = "https://www.nsandi.com/"
 
 CHAT_BUTTON_CSS = "#GCLauncherbtn"
-UNSUPPORTED_TEXT = "Chrome or Safari"
+UNSUPPORTED_CSS = "#unsupported-browser"
 
 
 async def is_warning_shown(client):
     await client.navigate(URL)
     client.await_css(CHAT_BUTTON_CSS, is_displayed=True)
-    return client.find_text(UNSUPPORTED_TEXT, is_displayed=True)
+    client.await_css(UNSUPPORTED_CSS)
+    return client.find_css(UNSUPPORTED_CSS, is_displayed=True)
 
 
 @pytest.mark.only_platforms("android")
 @pytest.mark.asyncio
 @pytest.mark.with_interventions
 async def test_enabled(client):
-    assert not await is_warning_shown(client)
+    assert await is_warning_shown(client)
 
 
 @pytest.mark.only_platforms("android")
