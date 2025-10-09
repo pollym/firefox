a7968b10d6ab9a6b6f482be44dd1c58a7a2a2f37
Cristina Horotan <chorotan@mozilla.com>
2025-10-09 03:16:05 +0300

    Revert "Bug 1993383 - Trigger a repaint of the window in the parent process after a GPU process crash. r=bradwerth" for causing build bustages at nsCocoaWindow.mm
    This reverts commit 91b75d161ff7c2a7de6d2cc01cefdbc269e8fd01.

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -391,9 +391,13 @@ class nsCocoaWindow final : public nsBaseWidget {
   bool WidgetPaintsBackground() override { return true; }
 
   void CreateCompositor(int aWidth, int aHeight) override;
+  static void FinishCreateCompositor(
+      int aWidth, int aHeight,
+      mozilla::ipc::Endpoint<mozilla::layers::PNativeLayerRemoteParent>&&
+          aParentEndpoint,
+      RefPtr<mozilla::layers::NativeLayerRootRemoteMacParent>
+          aNativeLayerRootRemoteMacParent);
   void DestroyCompositor() override;
-  void NotifyCompositorSessionLost(
-      mozilla::layers::CompositorSession* aSession) override;
   void SetCompositorWidgetDelegate(
       mozilla::widget::CompositorWidgetDelegate*) override;
 
