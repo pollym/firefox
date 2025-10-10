b464b81a7d686b9d3ca510b89da1a6b3a79957f1
Sam Sneddon <gsnedders@apple.com>
2025-10-10 07:49:32 +0000

    Bug 1992845 [wpt PR 55145] - Give wptrunner a distinct exit code for CRITICAL errors, a=testonly
    Automatic update from web-platform-tests
    Give wptrunner a distinct exit code for CRITICAL errors
    
    Previously, when running tests without --stability, wptrunner would
    exit with 1 if there were unexpected results or if there was a
    critical log message. With --stability, wptrunner would exit with
    either 1 or 2 for stability failures, and 1 if the stability check
    succeeded but there were critical log messages. In either case, wpt
    run would then simplify this to always exiting with 0 or 1.
    
    We now reserve exit codes above 64 for wptrunner's own usage, and exit
    with 64 if there is a critical log message. This now takes priority
    over the original exit code.
    
    We also now allow wpt run to exit with the exit code that wptrunner
    returns.
    
    --
    
    wpt-commits: 7902cfcbc75279eaff0e3874819ce954a213d761
    wpt-pr: 55145

--- testing/web-platform/tests/tools/wpt/run.py
+++ testing/web-platform/tests/tools/wpt/run.py
@@ -1012,7 +1012,7 @@ def run(venv, **kwargs):
     setup_cls, wptrunner_kwargs = setup_wptrunner(venv, **kwargs)
 
     try:
-        rv = run_single(venv, **wptrunner_kwargs) > 0
+        rv = run_single(venv, **wptrunner_kwargs)
     finally:
         setup_cls.teardown()
 
