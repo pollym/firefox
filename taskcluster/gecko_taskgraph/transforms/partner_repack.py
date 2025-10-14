f267f4acbf02b015a1fe0e558e59b1aaedb552e1
Heitor Neiva <hneiva@mozilla.com>
2025-10-14 15:44:01 +0000

    Bug 1990539 - Fix partner build signing with upstream artifacts r=releng-reviewers,taskgraph-reviewers,amadan
    In non-prod environments, the upstream task will be "signing", and on prod it will be "notarization".
    
    Differential Revision: https://phabricator.services.mozilla.com/D267718

--- taskcluster/gecko_taskgraph/transforms/partner_repack.py
+++ taskcluster/gecko_taskgraph/transforms/partner_repack.py
@@ -130,7 +130,11 @@ def add_command_arguments(config, tasks):
         task["worker"]["env"]["UPSTREAM_TASKIDS"] = {
             # We only want signing related tasks here, not build (used by mac builds for signing artifact resolution)
             "task-reference": " ".join(
-                [f"<{dep}>" for dep in task["dependencies"] if "signing" in dep]
+                [
+                    f"<{dep}>"
+                    for dep in task["dependencies"]
+                    if ("signing" in dep or "notarization" in dep)
+                ]
             )
         }
 
