67bbac0bc577525495399cbc70c60e9efab0f81d
Dan Baker <dbaker@mozilla.com>
2026-09-11 14:39:25 -0600

    Bug 2069067 - Vendor libwebrtc from 57f79e8e19
    Upstream commit: https://webrtc.googlesource.com/src/+/57f79e8e198c946f781606df091e733f3d033c5f
        Implement Audio Callback and Deprecate Audio GetSources
    
        This is step #2 of 5 of incrementally landing the changes from:
        https://webrtc-review.git.corp.google.com/c/src/+/494041/ (a CL that's
        pretty big as is).
    
        The overall goal of these CLs is to move the SourceTracker from
        ChannelReceive (audio) and VideoReceiveStream2 (video) into
        RtpReceiverBase and be accessible on the signaling thread rather than
        interfere with the worker (usually joined with network) thread. This
        offloads the worker/network thread for managing the SourceTracker and
        also moves the tracker closer to the API layer which it implements
        (getContributingSources).
    
        In this CL: Update ChannelReceive to trigger the callback when a frame
        is processed. Keep GetSources() functional in this layer for now so
        higher layers don't break.
    
        Bug: chromium:463591201, webrtc:42222117
        Change-Id: If6a72c424da7cb36f7dcfe500b5d59cb1c1e6ef5
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/497760
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#48447}

--- third_party/libwebrtc/audio/audio_receive_stream.cc
+++ third_party/libwebrtc/audio/audio_receive_stream.cc
@@ -21,6 +21,7 @@
 #include <vector>
 
 #include "absl/base/nullability.h"
+#include "absl/functional/any_invocable.h"
 #include "absl/strings/string_view.h"
 #include "api/audio/audio_frame.h"
 #include "api/audio/audio_mixer.h"
@@ -96,7 +97,8 @@ std::unique_ptr<voe::ChannelReceiveInterface> CreateChannelReceive(
       config.decoder_factory, std::move(config.frame_decryptor),
       config.crypto_options, std::move(config.frame_transformer),
       std::move(config.on_first_packet), config.rtp.rtcp_event_observer,
-      packet_router, config.rtp.local_ssrc);
+      packet_router, config.rtp.local_ssrc,
+      std::move(config.on_frame_delivered_callback));
 }
 }  // namespace
 
--- third_party/libwebrtc/audio/channel_receive.h
+++ third_party/libwebrtc/audio/channel_receive.h
@@ -33,6 +33,7 @@
 #include "api/frame_transformer_interface.h"
 #include "api/neteq/neteq_factory.h"
 #include "api/rtp_headers.h"
+#include "api/rtp_packet_infos.h"
 #include "api/scoped_refptr.h"
 #include "api/transport/rtp/rtp_source.h"
 #include "api/units/time_delta.h"
@@ -186,7 +187,11 @@ std::unique_ptr<ChannelReceiveInterface> CreateChannelReceive(
     absl::AnyInvocable<void(uint32_t ssrc) &&> on_first_packet,
     RtcpEventObserver* rtcp_event_observer,
     PacketRouter* absl_nonnull packet_router,
-    uint32_t local_ssrc);
+    uint32_t local_ssrc,
+    // Note: `on_frame_delivered_callback` is called synchronously from
+    // audio/packet processing threads and must be thread-safe and non-blocking.
+    absl::AnyInvocable<void(const RtpPacketInfos&, Timestamp) const>
+        on_frame_delivered_callback = nullptr);
 
 }  // namespace voe
 }  // namespace webrtc
