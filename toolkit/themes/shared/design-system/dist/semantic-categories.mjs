815baf80281bbf65b258008399f43a611a86a8ec
Mark Striemer <mstriemer@mozilla.com>
2026-09-14 16:04:37 +0000

    Bug 2064736 - Expose --font-size-small to chrome documents r=dao,morgan,desktop-theme-reviewers,webcompat-reviewers,twisniewski
    Provide `--font-size-small` in chrome documents and target its relative
    font size to be >= 11px. On macOS this means using 1rem, on Windows it's
    (11 / 12)rem and Linux is left unchanged at expected 12.72px (14.6719 * 0.867)
    
    Differential Revision: https://phabricator.services.mozilla.com/D319833

--- toolkit/themes/shared/design-system/dist/semantic-categories.mjs
+++ toolkit/themes/shared/design-system/dist/semantic-categories.mjs
@@ -2003,7 +2003,13 @@ export const tokensTable = {
       name: "--font-size-root",
     },
     {
-      value: { brand: { default: "0.867rem" }, platform: { default: "unset" } },
+      value: {
+        brand: { default: "0.867rem" },
+        platform: {
+          default: "1rem",
+          comment: "macOS 11px, Windows 12px, Linux overridden to >= 11px",
+        },
+      },
       name: "--font-size-small",
     },
     { value: "1.133rem", name: "--font-size-large" },
@@ -3996,7 +4002,10 @@ export const variableLookupTable = {
   },
   "font-size-small": {
     brand: { default: "0.867rem" },
-    platform: { default: "unset" },
+    platform: {
+      default: "1rem",
+      comment: "macOS 11px, Windows 12px, Linux overridden to >= 11px",
+    },
   },
   "font-size-large": "1.133rem",
   "font-size-xlarge": "1.467rem",
--- toolkit/themes/shared/design-system/dist/tokens-table.mjs
+++ toolkit/themes/shared/design-system/dist/tokens-table.mjs
@@ -1976,7 +1976,13 @@ export const tokensTable = {
       name: "--font-size-root",
     },
     {
-      value: { brand: { default: "0.867rem" }, platform: { default: "unset" } },
+      value: {
+        brand: { default: "0.867rem" },
+        platform: {
+          default: "1rem",
+          comment: "macOS 11px, Windows 12px, Linux overridden to >= 11px",
+        },
+      },
       name: "--font-size-small",
     },
     { value: "1.133rem", name: "--font-size-large" },
@@ -3998,7 +4004,10 @@ export const variableLookupTable = {
   },
   "font-size-small": {
     brand: { default: "0.867rem" },
-    platform: { default: "unset" },
+    platform: {
+      default: "1rem",
+      comment: "macOS 11px, Windows 12px, Linux overridden to >= 11px",
+    },
   },
   "font-size-large": "1.133rem",
   "font-size-xlarge": "1.467rem",
