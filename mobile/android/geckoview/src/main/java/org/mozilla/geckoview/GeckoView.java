5e9151c805e166de3fb96e8323c8843e2ffd7f36
owlishDeveloper <bugzeeeeee@gmail.com>
2026-09-14 15:01:49 +0000

    Bug 2057685 - Part 5: Add GeckoView API surface for obtaining full page screenshot r=geckoview-reviewers,ohall
    Differential Revision: https://phabricator.services.mozilla.com/D318617

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -266,6 +266,24 @@ public class GeckoView extends FrameLayout implements GeckoDisplay.NewSurfacePro
 
       return mDisplay.capturePixels();
     }
+
+    /**
+     * Request a {@link Bitmap} of the full web page currently being rendered (including areas
+     * outside the view)
+     *
+     * @return A {@link GeckoResult} that completes with a {@link Bitmap} containing the pixels and
+     *     size information of the currently rendered web page.
+     */
+    @UiThread
+    @NonNull
+    GeckoResult<Bitmap> captureFullPage() {
+      if (mDisplay == null) {
+        return GeckoResult.fromException(
+            new IllegalStateException("Display must be created before pixels can be captured"));
+      }
+
+      return mDisplay.captureFullPage();
+    }
   }
 
   /**
@@ -1045,6 +1063,20 @@ public class GeckoView extends FrameLayout implements GeckoDisplay.NewSurfacePro
   @UiThread
   public @NonNull GeckoResult<Bitmap> capturePixels() {
     return mDisplay.capturePixels();
+  }
+
+  /**
+   * Request a {@link Bitmap} of the full web page currently being rendered (including areas outside
+   * the view)
+   *
+   * <p>See {@link GeckoDisplay#captureFullPage} for more details.
+   *
+   * @return A {@link GeckoResult} that completes with a {@link Bitmap} containing the pixels and
+   *     size information of the currently rendered web page.
+   */
+  @UiThread
+  public @NonNull GeckoResult<Bitmap> captureFullPage() {
+    return mDisplay.captureFullPage();
   }
 
   /**
