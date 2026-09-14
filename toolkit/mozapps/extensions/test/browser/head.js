efdf4b0132f0cf289ca30fd36f32da0e4ba94987
Mark Banner <standard8@mozilla.com>
2026-09-14 09:33:29 +0000

    Bug 1710901 - Replace hand-rolled waitForCondition with TestUtils.waitForCondition in toolkit/mozapps/extensions/test/browser. r=extension-reviewers,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D325387

--- toolkit/mozapps/extensions/test/browser/head.js
+++ toolkit/mozapps/extensions/test/browser/head.js
@@ -1360,31 +1360,6 @@ MockInstall.prototype = {
   },
 };
 
-function waitForCondition(condition, nextTest, errorMsg) {
-  let tries = 0;
-  let interval = setInterval(function () {
-    if (tries >= 30) {
-      ok(false, errorMsg);
-      moveOn();
-    }
-    var conditionPassed;
-    try {
-      conditionPassed = condition();
-    } catch (e) {
-      ok(false, e + "\n" + e.stack);
-      conditionPassed = false;
-    }
-    if (conditionPassed) {
-      moveOn();
-    }
-    tries++;
-  }, 100);
-  let moveOn = function () {
-    clearInterval(interval);
-    nextTest();
-  };
-}
-
 /**
  * Wait for the given PopupNotification to display
  *
