49931ce023e4b21a85ed34bbf98d3aa0914c1b43
Atila Butkovits <abutkovits@mozilla.com>
2026-09-14 22:58:46 +0300

    Revert "Bug 2068765 - Part 2: Create notification from Declarative Web Push. r=saschanaz" for causing bustages in Declarative.cpp.
    This reverts commit 9e71bf401bd9673c331116e2ad9571378ac2efa8.
    
    Revert "Bug 2068765 - Part 1: Parser for Declarative Web Push. r=saschanaz"
    
    This reverts commit 49e681bb622c748f88a6e88be4c18db4456e4bfc.

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -27,7 +27,6 @@ extern crate cubeb_coreaudio;
 extern crate cubeb_pulse;
 extern crate data_storage;
 extern crate dom_fragmentdirectives;
-extern crate dom_push;
 extern crate dom_speculationrules;
 extern crate encoding_glue;
 extern crate fog_control;
