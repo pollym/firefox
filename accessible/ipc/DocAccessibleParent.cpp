ba6d3daaaf1c37f979c75922861ed818d15a4628
James Teh <jteh@mozilla.com>
2026-09-14 23:39:43 +0000

    Bug 2061691: When validating ranges in DocAccessibleParent::SelectionRanges, check that the start and end accs are HyperTextAccessibles. r=accessibility-platform-reviewers,eeejay
    As noted in the comment in this method, tree and selection updates happen in separate IPDL calls, so it's possible for a client selection query to arrive between them.
    We work around this by validating the Accessibles and offsets.
    Previously, we null checked the Accessibles.
    However, it's possible (albeit rare) that the same id is reused immediately when an Accessible is destroyed and another one is created.
    In this case, the id might now be associated with an Accessible that is not a HyperTextAccessible, thus causing a null deref in TextRange::Crop.
    To fix this, we now check IsHyperText() as well when validating selection ranges.
    
    A cleaner fix would be to remove any selection range that contains an Accessible that is being removed from the RemoteAccessible cache.
    However, I worry iterating selection ranges on every single removal could get expensive, especially in the (admittedly rare) case that there are a lot of noncontiguous selection ranges.
    Yet another alternative could be to somehow process selection range changes atomically with tree mutations, but I think this would be quite difficult to get right and would likely be plagued by obscure edge cases.
    
    Differential Revision: https://phabricator.services.mozilla.com/D325785

--- accessible/ipc/DocAccessibleParent.cpp
+++ accessible/ipc/DocAccessibleParent.cpp
@@ -1411,7 +1411,8 @@ void DocAccessibleParent::SelectionRanges(nsTArray<TextRange>* aRanges) const {
     auto* startAcc =
         const_cast<RemoteAccessible*>(GetAccessible(data.StartID()));
     auto* endAcc = const_cast<RemoteAccessible*>(GetAccessible(data.EndID()));
-    if (!startAcc || !endAcc) {
+    if (!startAcc || !endAcc || !startAcc->IsHyperText() ||
+        !endAcc->IsHyperText()) {
       continue;
     }
     // Offset 0 is always valid, even if the container is empty.
