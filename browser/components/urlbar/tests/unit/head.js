c35e3061b76a1743d6eb1b3db64558beb0587f2e
Dão Gottwald <dao@mozilla.com>
2026-09-15 09:35:32 +0000

    Bug 2071618 - Disable tab-to-search from the urlbar unit manifest instead of testEngine_setup. r=mak,urlbar-reviewers
    test_trimming.js and test_autofill_functional.js have their own add_setup and never called testEngine_setup, so they kept counting the MDN row.
    
    Differential Revision: https://phabricator.services.mozilla.com/D326106

--- browser/components/urlbar/tests/unit/head.js
+++ browser/components/urlbar/tests/unit/head.js
@@ -395,7 +395,6 @@ function testEngine_setup() {
 
     registerCleanupFunction(async () => {
       Services.prefs.clearUserPref("browser.urlbar.suggest.searches");
-      Services.prefs.clearUserPref("browser.urlbar.suggest.engines");
       Services.prefs.clearUserPref("browser.urlbar.contextualSearch.enabled");
       Services.prefs.clearUserPref(
         "browser.search.separatePrivateDefault.featureGate"
@@ -412,7 +411,6 @@ function testEngine_setup() {
       false
     );
     Services.prefs.setBoolPref("browser.urlbar.suggest.searches", false);
-    Services.prefs.setBoolPref("browser.urlbar.suggest.engines", false);
     Services.prefs.setBoolPref(
       "browser.urlbar.scotchBonnet.enableOverride",
       false
