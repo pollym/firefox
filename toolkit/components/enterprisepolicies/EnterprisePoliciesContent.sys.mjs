16f1aef104734dd2589cda324a1c470dc19837cc
Dave Townsend <dtownsend@oxymoronical.com>
2026-09-15 15:52:05 +0000

    Bug 2045801: Expand SitePolicies to support containerized site data. r=bsmth,baku,ipc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D323260

--- toolkit/components/enterprisepolicies/EnterprisePoliciesContent.sys.mjs
+++ toolkit/components/enterprisepolicies/EnterprisePoliciesContent.sys.mjs
@@ -77,6 +77,13 @@ export class EnterprisePoliciesManagerContent {
       uri
     );
   }
+
+  getContainerForURI(_uri) {
+    throw Components.Exception(
+      "getContainerForURI is not available in content processes",
+      Cr.NS_ERROR_NOT_IMPLEMENTED
+    );
+  }
 }
 
 EnterprisePoliciesManagerContent.prototype.QueryInterface =
