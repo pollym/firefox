9e4b64e2d7c1d6019008398e531b804050a6c079
Shravan Narayan <shravanrn@gmail.com>
2026-09-16 15:04:46 +0000

    Bug 2067517 - Update wasm2c compiler to new version r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D322130

--- third_party/rlbox_wasm2c_sandbox/src/wasm2c_rt_mem.c
+++ third_party/rlbox_wasm2c_sandbox/src/wasm2c_rt_mem.c
@@ -165,7 +165,7 @@ wasm_rt_memory_t create_wasm2c_memory(uint32_t initial_pages,
 
   wasm_rt_memory_t ret;
   ret.data = data;
-  //ret.page_size = WASM_PAGE_SIZE;
+  ret.page_size = WASM_PAGE_SIZE;
   ret.pages = initial_pages;
   ret.max_pages = chosen_max_pages;
   ret.size = byte_length;
