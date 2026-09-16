0ddf76e37ee1a3c6b3a0fb3321e1bde49005106e
Abel Kalu <59095867+BlessedOneKobo@users.noreply.github.com>
2026-09-16 12:40:12 +0000

    Bug 2067180 - Remove unused WebsiteFilter module getter from browser.js. r=firefox-desktop-core-reviewers ,frontend-codestyle-reviewers,mossop
    We are removing WebsiteFilter from the module getters declared in browser/base/content/browser.js
    because it is no longer used. It was initially added to disable "Save Link As" for links blocked
    by the WebsiteFilter enterprise policy. That check was moved to nsContextMenu.sys.mjs which
    imports WebsiteFilter itself
    
    Differential Revision: https://phabricator.services.mozilla.com/D322573

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -124,7 +124,6 @@ ChromeUtils.defineESModuleGetters(this, {
   Weave: "resource://services-sync/main.sys.mjs",
   WebNavigationFrames: "resource://gre/modules/WebNavigationFrames.sys.mjs",
   webrtcUI: "resource:///modules/webrtcUI.sys.mjs",
-  WebsiteFilter: "resource:///modules/policies/WebsiteFilter.sys.mjs",
   ZoomUI: "resource:///modules/ZoomUI.sys.mjs",
 });
 
