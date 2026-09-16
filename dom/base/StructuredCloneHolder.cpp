f7e3aeb234aff0d9c18948cf066b24206ac7700e
Nika Layzell <nika@thelayzells.com>
2026-09-16 19:09:13 +0000

    Bug 2071727 - Perform CustomFreeTransferHandler bounds checks outside of fuzzing, r=dom-core-reviewers,mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D326661

--- dom/base/StructuredCloneHolder.cpp
+++ dom/base/StructuredCloneHolder.cpp
@@ -1884,12 +1884,9 @@ void StructuredCloneHolder::CustomFreeTransferHandler(
 
   if (aTag == SCTAG_DOM_MAP_MESSAGEPORT) {
     MOZ_ASSERT(!aContent);
-#ifdef FUZZING
     if (aExtraData >= mPortIdentifiers.Length()) {
       return;
     }
-#endif
-    MOZ_ASSERT(aExtraData < mPortIdentifiers.Length());
     MessagePort::ForceClose(mPortIdentifiers[aExtraData]);
     return;
   }
@@ -1913,24 +1910,18 @@ void StructuredCloneHolder::CustomFreeTransferHandler(
 
   if (aTag == SCTAG_DOM_READABLESTREAM || aTag == SCTAG_DOM_WRITABLESTREAM) {
     MOZ_ASSERT(!aContent);
-#ifdef FUZZING
     if (aExtraData >= mPortIdentifiers.Length()) {
       return;
     }
-#endif
-    MOZ_ASSERT(aExtraData < mPortIdentifiers.Length());
     MessagePort::ForceClose(mPortIdentifiers[aExtraData]);
     return;
   }
 
   if (aTag == SCTAG_DOM_TRANSFORMSTREAM) {
     MOZ_ASSERT(!aContent);
-#ifdef FUZZING
     if (aExtraData + 1 >= mPortIdentifiers.Length()) {
       return;
     }
-#endif
-    MOZ_ASSERT(aExtraData + 1 < mPortIdentifiers.Length());
     MessagePort::ForceClose(mPortIdentifiers[aExtraData]);
     MessagePort::ForceClose(mPortIdentifiers[aExtraData + 1]);
     return;
