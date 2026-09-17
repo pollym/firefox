1226b3963f72428bbd780cc89a1ff79e4ecdeba9
Yubin Jamora <yjamora@mozilla.com>
2026-09-17 17:27:10 +0000

    Bug 2057573 - Smart Window Monitor feature callout r=fluent-reviewers,omc-reviewers,bolsson,sachung
    Differential Revision: https://phabricator.services.mozilla.com/D325515

--- toolkit/components/messaging-system/lib/SpecialMessageActions.sys.mjs
+++ toolkit/components/messaging-system/lib/SpecialMessageActions.sys.mjs
@@ -392,6 +392,7 @@ export const SpecialMessageActions = {
     const allowedPrefs = [
       "browser.aboutwelcome.didSeeFinalScreen",
       "browser.sessionstore.newTabOnRestore",
+      "browser.smartwindow.agent.monitorAnnouncement",
       "browser.smartwindow.enabled",
       "browser.smartwindow.firstrun.hasCompleted",
       "browser.smartwindow.firstrun.modelChoice",
@@ -1064,6 +1065,9 @@ export const SpecialMessageActions = {
           false,
           action.data?.source ?? "asrouter"
         );
+      case "OPEN_SMARTWINDOW_MONITOR_CREATE":
+        lazy.AIWindowUI.showMonitorCreateForm(window);
+        break;
       case "OPEN_PROTECTION_PANEL": {
         let { gProtectionsHandler } = window;
         gProtectionsHandler.showProtectionsPopup({});
