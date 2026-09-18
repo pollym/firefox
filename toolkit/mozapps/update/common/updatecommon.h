408905d24d2bab4f53d305785596bc4dd9d3ba4a
Atila Butkovits <abutkovits@mozilla.com>
2026-09-18 10:11:37 +0300

    Revert "Bug 2037979 - Improve logging of updater invocation scenarios r=application-update-reviewers,aborondo" for causing bustages in elevation_type.cpp.
    This reverts commit 2f4e1be33d5f7046e30a09e4c647897c11019c01.

--- toolkit/mozapps/update/common/updatecommon.h
+++ toolkit/mozapps/update/common/updatecommon.h
@@ -37,7 +37,6 @@ bool IsValidFullPath(NS_tchar* fullPath);
 bool IsProgramFilesPath(NS_tchar* fullPath);
 FILE* CreateAndOpenFile(NS_tchar* logFilePath, bool binary);
 void LogToOS(const NS_tchar* message);
-bool EnvHasValue(const char* name);
 
 #define LOG_WARN(args) UpdateLog::GetPrimaryLog().WarnPrintf args
 #define LOG(args) UpdateLog::GetPrimaryLog().Printf args
