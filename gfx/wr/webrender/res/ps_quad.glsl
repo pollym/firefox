d3c188d713c37ac149c47c453544fcb66dcefc54
agoloman <agoloman@mozilla.com>
2026-09-18 20:08:51 +0300

    Revert "Bug 2072568 - Anti-alias horizontal edges in SWGL. r=lsalzman" for causing wr failures.
    This reverts commit 5a5f4660b5d019f54a75e3c935db66285b97c12d.

--- gfx/wr/webrender/res/ps_quad.glsl
+++ gfx/wr/webrender/res/ps_quad.glsl
@@ -277,9 +277,7 @@ PrimitiveInfo quad_primive_info(void) {
         case PART_LEFT:
             local_coverage_rect.p1.x = local_coverage_rect.p0.x + AA_PIXEL_RADIUS;
 #ifdef SWGL_ANTIALIAS
-            // The strip covers the full height of the primitive, so its top
-            // and bottom rows lie on the primitive's top and bottom edges.
-            swgl_antiAlias(EDGE_AA_LEFT | (qi.edge_flags & (EDGE_AA_TOP | EDGE_AA_BOTTOM)));
+            swgl_antiAlias(EDGE_AA_LEFT);
 #else
             local_coverage_rect.p0.x -= AA_PIXEL_RADIUS;
             local_coverage_rect.p0.y -= aa_top * AA_PIXEL_RADIUS;
@@ -299,9 +297,7 @@ PrimitiveInfo quad_primive_info(void) {
         case PART_RIGHT:
             local_coverage_rect.p0.x = local_coverage_rect.p1.x - AA_PIXEL_RADIUS;
 #ifdef SWGL_ANTIALIAS
-            // The strip covers the full height of the primitive, so its top
-            // and bottom rows lie on the primitive's top and bottom edges.
-            swgl_antiAlias(EDGE_AA_RIGHT | (qi.edge_flags & (EDGE_AA_TOP | EDGE_AA_BOTTOM)));
+            swgl_antiAlias(EDGE_AA_RIGHT);
 #else
             local_coverage_rect.p1.x += AA_PIXEL_RADIUS;
             local_coverage_rect.p0.y -= aa_top * AA_PIXEL_RADIUS;
