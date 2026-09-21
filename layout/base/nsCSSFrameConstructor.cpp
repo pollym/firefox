057d1e9e61b63926dac04a88853ef2a1e115b82a
Tiaan Louw <tlouw@mozilla.com>
2026-09-21 09:15:06 +0000

    Bug 1995348 - Implement the "grid-lanes" value for the "display" property r=layout-reviewers,firefox-style-system-reviewers,dholbert,emilio
    Use the "display: grid-lanes" value to conform to current usage in web
    platform tests.
    
    Two tests are marked as CRASH, because the masonry code uses a sort
    comparitor that is not valid strick weak ordering as required, so it
    asserts in debug builds. See bug 2068688.
    
    See: https://drafts.csswg.org/css-grid-3/#masonry-model
    
    Differential Revision: https://phabricator.services.mozilla.com/D273845

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -2447,9 +2447,11 @@ nsIFrame* nsCSSFrameConstructor::ConstructDocElementFrame(
         state, item, mDocElementContainingBlock, display, frameList));
   } else if (display->mDisplay == StyleDisplay::Flex ||
              display->mDisplay == StyleDisplay::WebkitBox ||
-             display->mDisplay == StyleDisplay::Grid) {
+             display->mDisplay == StyleDisplay::Grid ||
+             display->mDisplay == StyleDisplay::GridLanes) {
     auto func = [&] {
-      if (display->mDisplay == StyleDisplay::Grid) {
+      if (display->mDisplay == StyleDisplay::Grid ||
+          display->mDisplay == StyleDisplay::GridLanes) {
         return NS_NewGridContainerFrame;
       }
       return NS_NewFlexContainerFrame;
@@ -4258,7 +4260,8 @@ nsCSSFrameConstructor::FindDisplayData(const nsStyleDisplay& aDisplay,
       return MOZ_UNLIKELY(propagatedScrollToViewport) ? &nonScrollableData
                                                       : &data;
     }
-    case StyleDisplayInside::Grid: {
+    case StyleDisplayInside::Grid:
+    case StyleDisplayInside::GridLanes: {
       static constexpr FrameConstructionData nonScrollableData(
           ToCreationFunc(NS_NewGridContainerFrame));
       static constexpr FrameConstructionData data(
