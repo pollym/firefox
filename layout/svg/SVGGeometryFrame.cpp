ea380a182327e2d819ad9951d9af833f7dba8499
longsonr <longsonr@gmail.com>
2026-09-21 08:24:12 +0000

    Bug 2073778 - Use estimate of stroke bounds for reflow r=dholbert,firefox-svg-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D327604

--- layout/svg/SVGGeometryFrame.cpp
+++ layout/svg/SVGGeometryFrame.cpp
@@ -267,9 +267,11 @@ void SVGGeometryFrame::ReflowSVG() {
     return;
   }
 
+  // Speed is more important than accuracy here - we don't care if the stroke
+  // bounds are slightly too large so we set EstimateStrokeBounds.
   SVGBBoxFlags flags = {SVGBBoxFlag::IncludeFillGeometry,
-                        SVGBBoxFlag::IncludeStroke,
-                        SVGBBoxFlag::IncludeMarkers};
+                        SVGBBoxFlag::IncludeStroke, SVGBBoxFlag::IncludeMarkers,
+                        SVGBBoxFlag::EstimateStrokeBounds};
 
   // Our "visual" overflow rect needs to be valid for building display lists
   // for hit testing, which means that for certain values of 'pointer-events'
@@ -448,7 +450,8 @@ SVGBBox SVGGeometryFrame::GetBBoxContribution(const Matrix& aToBBoxUserspace,
       //   stroke bounds that it will return will be empty.
 
       Maybe<Rect> strokeBBoxExtents;
-      if (StaticPrefs::svg_Moz2D_strokeBounds_enabled()) {
+      if (!aFlags.contains(SVGBBoxFlag::EstimateStrokeBounds) &&
+          StaticPrefs::svg_Moz2D_strokeBounds_enabled()) {
         if (userToOuterSVG) {
           Matrix m = ToMatrix(*userToOuterSVG);
           Matrix outerSVGToBBox = aToBBoxUserspace * m.Inverse();
