2ab13412cede169981076ab0bfed4aca189d26f3
Mark Banner <standard8@mozilla.com>
2026-09-21 13:11:19 +0000

    Bug 2071446 - Fix test failure in browser_localSearchShortcuts_xul.js caused when too many search engines are listed. r=settings-reviewers,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D327419

--- browser/components/preferences/tests/search/browser_localSearchShortcuts_xul.js
+++ browser/components/preferences/tests/search/browser_localSearchShortcuts_xul.js
@@ -8,6 +8,7 @@
 "use strict";
 
 ChromeUtils.defineESModuleGetters(this, {
+  SearchTestUtils: "resource://testing-common/SearchTestUtils.sys.mjs",
   UrlbarPrefs: "moz-src:///browser/components/urlbar/UrlbarPrefs.sys.mjs",
   UrlbarShared: "chrome://browser/content/urlbar/UrlbarShared.mjs",
   UrlbarUtils: "moz-src:///browser/components/urlbar/UrlbarUtils.sys.mjs",
@@ -20,6 +21,9 @@ const isRestrictKeywordsFeatureOn = () =>
   UrlbarPrefs.getScotchBonnetPref("searchRestrictKeywords.featureGate");
 
 add_setup(async function () {
+  // Use a single application provided engine, so that the xul table does not
+  // overflow with too many search engines.
+  await SearchTestUtils.updateRemoteSettingsConfig([{ identifier: "engine1" }]);
   await SpecialPowers.pushPrefEnv({
     set: [["browser.urlbar.scotchBonnet.enableOverride", true]],
   });
