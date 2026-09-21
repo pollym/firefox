e251bb1ebbf66b6297b6551d8ef2037c6c6e5d11
Nick Alexander <nalexander@mozilla.com>
2026-09-21 22:46:51 +0000

    Bug 1794663 - Make `concept-engine` not depend on `browser-errorpages`. r=android-reviewers,petru
    This is fairly straightforward:
    
    * we lift the `ErrorType` definition up to `concept-engine`;
    * we make `browser-errorpages` expose the mapping from `ErrorType` to
      concrete strings encapsulated in an `ErrorStrings` type via an
      `ErrorStringsProvider` abstraction;
    * we implement a `DefaultErrorStringsProvider` that can be replaced or
      extended by consumers.
    
    Along the way, we replace some `*Override` functions by using the
    provider.  And we harmonize how optional parts of the error page (for
    archiving, for certificate errors, and continuing to HTTP sites) work
    by introducing optional `*Details` objects to augment `ErrorStrings`.
    
    This work was scoped to not change the underlying error page HTML.  As
    a result, the detail augmentation approach is not particularly
    graceful.  If we wanted to invest further, we might expose a more
    general interface to the error page content that is less tied to
    specific errors, and reduce the special cases inherent in both the
    HTML layer and the JVM layer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D295734

--- mobile/android/android-components/components/browser/errorpages/src/test/java/mozilla/components/browser/errorpages/ErrorPagesTest.kt
+++ mobile/android/android-components/components/browser/errorpages/src/test/java/mozilla/components/browser/errorpages/ErrorPagesTest.kt
@@ -4,8 +4,10 @@
 
 package mozilla.components.browser.errorpages
 
+import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.components.browser.errorpages.ErrorPages.createUrlEncodedErrorPage
+import mozilla.components.concept.engine.request.ErrorType
 import mozilla.components.support.ktx.kotlin.urlEncode
 import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
@@ -93,14 +95,18 @@ class ErrorPagesTest {
                 testContext,
                 ErrorType.ERROR_HTTPS_ONLY,
                 "https://localhost/",
-                titleOverride = { errorType ->
-                    assertEquals(ErrorType.ERROR_HTTPS_ONLY, errorType)
-                    "radio"
-                },
-                descriptionOverride = { errorType ->
-                    assertEquals(ErrorType.ERROR_HTTPS_ONLY, errorType)
-                    "spider"
-                },
+                errorStringsProvider =
+                    object : DefaultErrorStringsProvider() {
+                        override fun errorStringsFor(
+                            context: Context,
+                            errorType: ErrorType,
+                            uri: String?,
+                        ): ErrorStrings {
+                            assertEquals(ErrorType.ERROR_HTTPS_ONLY, errorType)
+                            return super.errorStringsFor(context, errorType, uri)
+                                .copy(title = "radio", message = "spider")
+                        }
+                    },
             )
 
         assertTrue(customErrorPage.contains("radio"))
@@ -175,6 +181,24 @@ class ErrorPagesTest {
     }
 
     @Test
+    fun `createUrlEncodedErrorPage interpolates uri param when required`() {
+        listOf(
+                "https://example1.org",
+                "http://example2.com/", // Note trailing slash.
+            )
+            .forEach { uri ->
+                val page =
+                    createUrlEncodedErrorPage(
+                        testContext,
+                        ErrorType.ERROR_BAD_HSTS_CERT,
+                        uri,
+                    )
+                assertTrue("generated page contains '${uri}' (encoded)", page.contains(uri.urlEncode()))
+                assertFalse("generated page does not contain uninterpolated strings", page.contains("\$s"))
+            }
+    }
+
+    @Test
     fun `createUrlEncodedErrorPage omits archive params when the action is disabled`() {
         val page =
             createUrlEncodedErrorPage(
@@ -200,20 +224,18 @@ class ErrorPagesTest {
                 htmlFilename,
             )
 
+        val errorStrings = DefaultErrorStringsProvider().errorStringsFor(testContext, errorType, uri)
         val expectedImageName =
-            if (errorType.imageNameRes != null) {
-                testContext.resources.getString(errorType.imageNameRes) + ".svg"
+            if (errorStrings.imageName != null) {
+                errorStrings.imageName + ".svg"
             } else {
                 ""
             }
 
         assertTrue(errorPage.startsWith("resource://android/assets/$htmlFilename"))
-        assertTrue(
-            errorPage.contains("&button=${testContext.resources.getString(errorType.refreshButtonRes).urlEncode()}")
-        )
+        assertTrue(errorPage.contains("&button=${errorStrings.refreshButton.urlEncode()}"))
 
-        val description =
-            testContext.resources.getString(errorType.messageRes, uri).replace("<ul>", "<ul role=\"presentation\">")
+        val description = errorStrings.message.replace("<ul>", "<ul role=\"presentation\">")
 
         assertTrue(errorPage.contains("&description=${description.urlEncode()}"))
         assertTrue(errorPage.contains("&image=$expectedImageName"))
