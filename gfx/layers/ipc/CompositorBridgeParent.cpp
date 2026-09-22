b09d7949a50abc8819a06eb67a2c78e6389e3ba0
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2026-09-22 13:20:48 +0000

    Bug 2072511 - Add support of headless mode with GPU process r=geckoview-reviewers,gtk-reviewers,gfx-reviewers,emilio,owlish,nical
    On Android, gecko already tries to use HeadlessCompositorWidget in GPU process during gtest.It is done without setting "MOZ_HEADLESS=1" on Android. And caused crashes during gtest. See Bug 2072484.
    
    The change addresses the crash.
    
    Differential Revision: https://phabricator.services.mozilla.com/D326389

--- gfx/layers/ipc/CompositorBridgeParent.cpp
+++ gfx/layers/ipc/CompositorBridgeParent.cpp
@@ -60,6 +60,7 @@
 #include "mozilla/mozalloc.h"                          // for operator new, etc
 #include "mozilla/webrender/RenderThread.h"
 #include "mozilla/webrender/WebRenderAPI.h"
+#include "mozilla/widget/HeadlessCompositorWidgetParent.h"
 #include "nsCOMPtr.h"         // for already_AddRefed
 #include "nsDebug.h"          // for NS_ASSERTION, etc
 #include "nsISupportsImpl.h"  // for MOZ_COUNT_CTOR, etc
@@ -1521,6 +1522,14 @@ CompositorBridgeParent::AllocPCompositorWidgetParent(
     return nullptr;
   }
 
+  if (aInitData.type() ==
+      CompositorWidgetInitData::THeadlessCompositorWidgetInitData) {
+    RefPtr widget = MakeRefPtr<widget::HeadlessCompositorWidgetParent>(
+        aInitData.get_HeadlessCompositorWidgetInitData(), mOptions);
+    mWidget = widget;
+    return widget.forget();
+  }
+
   RefPtr widget =
       MakeRefPtr<widget::CompositorWidgetParent>(aInitData, mOptions);
 
@@ -1536,6 +1545,10 @@ CompositorBridgeParent::AllocPCompositorWidgetParent(
 mozilla::ipc::IPCResult
 CompositorBridgeParent::RecvPCompositorWidgetConstructor(
     PCompositorWidgetParent* actor, CompositorWidgetInitData&& aInitData) {
+  if (aInitData.type() ==
+      CompositorWidgetInitData::THeadlessCompositorWidgetInitData) {
+    return IPC_OK();
+  }
   // macOS CocoaCompositorWidget (a superclass of the platform-specific
   // CompositorWidgetParent) requires an extra step to pass aInitData
   // with move semantics, because IPDL can't generate move semantics
