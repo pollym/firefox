91b817d70c381be381cfc282065c5fea86a605bb
Lee Salzman <lsalzman@mozilla.com>
2026-09-22 06:40:36 +0000

    Bug 2073969. r=gfx-reviewers,tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D328048

--- dom/canvas/OffscreenCanvas.cpp
+++ dom/canvas/OffscreenCanvas.cpp
@@ -213,7 +213,12 @@ void OffscreenCanvas::GetContext(
   // if we need to flush our contents to its ImageContainer for display.
   RefPtr<ThreadSafeWorkerRef> workerRef;
   if (mDisplay) {
-    if (WorkerPrivate* workerPrivate = GetCurrentThreadWorkerPrivate()) {
+    WorkerPrivate* workerPrivate = GetCurrentThreadWorkerPrivate();
+    if (!mDisplay->MayUpdateContext(workerPrivate, aRv)) {
+      aResult.SetNull();
+      return;
+    }
+    if (workerPrivate) {
       RefPtr<StrongWorkerRef> strongRef = StrongWorkerRef::Create(
           workerPrivate, "OffscreenCanvas::GetContext",
           [display = mDisplay]() { display->DestroyCanvas(); });
@@ -233,10 +238,6 @@ void OffscreenCanvas::GetContext(
       aCx, contextType, aContextOptions, aRv);
   if (!result) {
     aResult.SetNull();
-    if (mDisplay) {
-      mDisplay->UpdateContext(this, nullptr, CanvasContextType::NoContext,
-                              Nothing());
-    }
     return;
   }
 
