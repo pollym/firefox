664e8a12ba6f9279a4d2d23c9fc90ee4f011c5fc
unifolia <jlewis@mozilla.com>
2026-09-22 04:26:39 +0000

    Bug 2063562 - Don't count a restoring about:blank tab as a duplicate. r=tabbrowser-reviewers,jswinarton
    Differential Revision: https://phabricator.services.mozilla.com/D327453

--- browser/components/tabbrowser/Tabbrowser.sys.mjs
+++ browser/components/tabbrowser/Tabbrowser.sys.mjs
@@ -5588,13 +5588,31 @@ export class Tabbrowser {
     return tabsToEnd;
   }
 
+  /**
+   * A restoring browser sits on about:blank until its page commits, so its
+   * URI says nothing about duplicates yet.
+   *
+   * @param {MozTabbrowserTab} tab
+   * @returns {nsIURI|null}
+   */
+  #uriForDuplicateCheck(tab) {
+    let uri = tab.linkedBrowser?.currentURI;
+    if (!uri) {
+      return null;
+    }
+    if (uri.spec == "about:blank" && lazy.SessionStore.isTabRestoring(tab)) {
+      return null;
+    }
+    return uri;
+  }
+
   getDuplicateTabsToClose(aTab) {
     // One would think that a set is better, but it would need to copy all
     // the strings instead of just keeping references to the nsIURI objects,
     // and the array is presumed to be small anyways.
     let keys = [];
     let keyForTab = tab => {
-      let uri = tab.linkedBrowser?.currentURI;
+      let uri = this.#uriForDuplicateCheck(tab);
       if (!uri) {
         return null;
       }
@@ -5654,7 +5672,7 @@ export class Tabbrowser {
     /** @type {Map<string, Set<number>>} */
     let userContextIdsPerUri = new Map();
     for (let tab of lastSeenTabs) {
-      const uri = tab.linkedBrowser?.currentURI;
+      const uri = this.#uriForDuplicateCheck(tab);
       if (!uri) {
         // Can't tell if it's a duplicate without a URI.
         // Safest to leave it be.
