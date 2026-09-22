e19a8107bca95be9abbc56f18a400e10c684fdda
Nicolas Silva <nsilva@mozilla.com>
2026-09-22 13:42:56 +0000

    Bug 2050211 - Add a wrench test. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D310362

--- gfx/wr/wrench/src/yaml_frame_reader.rs
+++ gfx/wr/wrench/src/yaml_frame_reader.rs
@@ -1767,6 +1767,7 @@ impl YamlFrameReader {
                 ("scrollbar-container", PrimitiveFlags::IS_SCROLLBAR_CONTAINER),
                 ("prefer-compositor-surface", PrimitiveFlags::PREFER_COMPOSITOR_SURFACE),
                 ("checkerboard-background", PrimitiveFlags::CHECKERBOARD_BACKGROUND),
+                ("antialiased", PrimitiveFlags::ANTIALISED),
                 ("rasterized-for-rect", PrimitiveFlags::RASTERIZED_FOR_RECT),
             ] {
                 if let Some(value) = item[key].as_bool() {
