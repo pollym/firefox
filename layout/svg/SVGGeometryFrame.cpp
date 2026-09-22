226150483e6386c7fd5a3dd627962e298d517e20
Atila Butkovits <abutkovits@mozilla.com>
2026-09-22 11:31:44 +0300

    Revert "Bug 2050211: apply code formatting via Lando" for causing failures at svg-no-natural-size-grandchild.html.
    This reverts commit 7473834b83efa1e64fae9faaaa1d5d284f42af24.
    
    Revert "Bug 2050211 - Honour shape-rendering: crispEdges for WebRender SVG rects. r=firefox-svg-reviewers,longsonr"
    
    This reverts commit 3151415a427681ca5a824675de6c6b75656a13a0.
    
    Revert "Bug 2050211 - Quantize reference frame origins in device space, not local space. r=gw"
    
    This reverts commit f2b230ac43905ea8bd4018ab93873b55f0253316.
    
    Revert "Bug 2050211 - Add a wrench test. r=gfx-reviewers,lsalzman"
    
    This reverts commit 04303baa631a3f824eb122bac7c95c0a33c67a7c.
    
    Revert "Bug 2050211 - Dont't snap anti-aliased primitives. r=gw"
    
    This reverts commit 657ba97309b215a3ead6526605c2642167ff6a9e.
    
    Revert "Bug 2050211 - Wire up AA for primitives that have the ANTIALIASING flag. r=gfx-reviewers,lsalzman"
    
    This reverts commit b1022dc0f55a1b2a25ba7d8d216d27a48714db4d.
    
    Revert "Bug 2050211 - Make test 1780191-1.svg not rely on WebRender's incorrect snapping of SVG rectangles. r-#gfx-reviewers r=layout-reviewers,tnikkel"
    
    This reverts commit c17facc81b75ee75ca7d86021eef625de3403ab8.

--- layout/svg/SVGGeometryFrame.cpp
+++ layout/svg/SVGGeometryFrame.cpp
@@ -757,13 +757,8 @@ WebRenderCommandsResult SVGGeometryFrame::CreateWebRenderCommands(
     auto color = wr::ToColorF(
         ToDeviceColor(StyleSVG()->mFill.kind.AsColor().CalcColor(this)));
     color.a *= opacity;
-
-    // shape-rendering: crispEdges / optimizeSpeed disables anti-aliasing, the
-    // same way it does for the gfxContext path in Render().
-    const bool antialiased = SVGUtils::ToAntialiasMode(
-                                 style->mShapeRendering) != AntialiasMode::NONE;
-    aBuilder.PushRect(wrRect, wrRect, !aItem->BackfaceIsHidden(), antialiased,
-                      false, color);
+    aBuilder.PushRect(wrRect, wrRect, !aItem->BackfaceIsHidden(), true, false,
+                      color);
   }
 
   return Ok();
