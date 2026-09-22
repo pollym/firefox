f3aca13478444bee3811648ae776b60ec3db8f36
Iain Ireland <iireland@mozilla.com>
2026-09-22 20:20:44 +0000

    Bug 2073681: Report OOM in WasmFunctionScope::create r=spidermonkey-reviewers,jpages
    The only other caller of debugGetLocalTypes is DebugFrame::getLocal, which in turn is only called by DebugEnvironmentProxyHandler::handleUnaliasedAccess, which [already reports OOM](https://searchfox.org/firefox-main/rev/73f46f2636b5677d7282764dd7afeaea7036f0ce/js/src/vm/EnvironmentObject.cpp#1973).
    
    Differential Revision: https://phabricator.services.mozilla.com/D327880

--- js/src/vm/Scope.cpp
+++ js/src/vm/Scope.cpp
@@ -776,6 +776,7 @@ WasmFunctionScope* WasmFunctionScope::create(JSContext* cx,
   wasm::StackResults unusedStackResults;
   if (!instance->instance().debug().debugGetLocalTypes(
           funcIndex, &locals, &argsLength, &unusedStackResults)) {
+    ReportOutOfMemory(cx);
     return nullptr;
   }
   uint32_t namesCount = locals.length();
