f1d4f5dc1fc9838bf78cd31cd270b3bf6911e31e
Matthew Tighe <matthewdtighe@gmail.com>
2026-09-22 18:20:52 +0000

    Bug 2064871 - Seek to a position in the article r=android-reviewers,moyin
    Differential Revision: https://phabricator.services.mozilla.com/D328018

--- mobile/android/android-components/components/feature/listentopage/src/main/java/mozilla/components/feature/listentopage/fakes/Listen.kt
+++ mobile/android/android-components/components/feature/listentopage/src/main/java/mozilla/components/feature/listentopage/fakes/Listen.kt
@@ -142,6 +142,8 @@ class FakePlaybackController(var positionMs: Long = 0L) : PlaybackController {
     val displayDataList = mutableListOf<ArticleDisplayData>()
     var resumed = 0
     val seekedTo = mutableListOf<Long>()
+    val seekedToItem = mutableListOf<Pair<Int, Long>>()
+    val restartedAt = mutableListOf<Pair<File, Long>>()
     var released = false
 
     override val status = MutableStateFlow(PlaybackState())
@@ -171,6 +173,15 @@ class FakePlaybackController(var positionMs: Long = 0L) : PlaybackController {
 
     override suspend fun seekTo(positionMs: Long) {
         seekedTo.add(positionMs)
+    }
+
+    override suspend fun seekTo(itemIndex: Int, positionMs: Long) {
+        seekedToItem.add(itemIndex to positionMs)
+    }
+
+    override suspend fun restartAt(file: File, positionMs: Long) {
+        restartedAt.add(file to positionMs)
+        played.add(file)
     }
 
     override suspend fun release() {
