d7d28d3dded0021e6791676c9d78c4fcda921ee0
Cosmin Sabou <csabou@mozilla.com>
2026-09-23 06:05:11 +0300

    Revert "Bug 2074592 - fix(pbw): Relay promo and basics spotlight fixes from QA r=omc-reviewers,emcminn" for causing node failures related to asrouter-newtab-multistage.
    This reverts commit 853e8033cafe52ac8a16c5e6828e66e184d3c9d7.

--- toolkit/modules/BrowserUtils.sys.mjs
+++ toolkit/modules/BrowserUtils.sys.mjs
@@ -954,7 +954,6 @@ let PromoInfo = {
     illegalRegions: [],
   },
   [BrowserUtils.PromoType.RELAY]: {
-    enabledPref: "browser.promo.relay.enabled",
     lazyStringSetPrefs: {},
     illegalRegions: [],
     // Returns true if user is using the FxA "production" instance, or returns
