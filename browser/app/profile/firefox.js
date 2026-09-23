906a45bc27784742fede9457c2111c8e4c2519b3
Dan Schomburg <dschomburg@mozilla.com>
2026-09-23 00:37:05 +0000

    Bug 2074307 - Default identity.fxaccounts.pairing.version to 2  r=markh
    We are preparing for the FxA pairing improvements rollout, also known as v2 pairing. To do so, we need to set the default pref for identity.fxaccounts.pairing.version to 2. With this set, FxA will be able to controll the rollout of the pairing flows through their server configurations. At the time of writing, pairing v2 has not yet been enabled in FxA sever configurations. We are targeting the end of the month to start rolling the v2 improvements so Android users can take advantage of the improvments. iOS users will follow a few weeks later.
    
    For pairing v2 to be turned on, both FxA's configuration and Firefox pairing v2 pref must be turned on. This commit enables pairing v2 by default on the Firefox side only! QA has validated that this is safe to do. Even with this pref set to 2, pairing v1 flows continue to work as intended. This valided last week against train v1.345.1.
    
    Differential Revision: https://phabricator.services.mozilla.com/D328013

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2501,7 +2501,7 @@ pref("identity.fxaccounts.remote.oauth.uri", "https://oauth.accounts.firefox.com
 pref("identity.fxaccounts.pairing.enabled", true);
 
 // The version of the pairing flow to be used by FxA.
-pref("identity.fxaccounts.pairing.version", 1);
+pref("identity.fxaccounts.pairing.version", 2);
 
 // The remote URI of the FxA pairing server
 pref("identity.fxaccounts.remote.pairing.uri", "wss://channelserver.services.mozilla.com");
