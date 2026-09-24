4d27cc793aa9a0139029d52441eda79456c786df
Sandor Molnar <smolnar@mozilla.com>
2026-09-24 17:11:45 +0300

    Revert "Bug 1995348 - Implement the "grid-lanes" value for the "display" property r=layout-reviewers,firefox-style-system-reviewers,dholbert,emilio" WR failures
    This reverts commit 0cee93c68c01ca99d244e328f2548e4241910f1d.

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -8798,6 +8798,7 @@ nscoord nsGridContainerFrame::MasonryLayout(GridReflowInput& aGridRI,
   // Collect our grid items and sort them in grid order.
   nsTArray<GridItemInfo*> sortedItems(aGridRI.mGridItems.Length());
   aGridRI.mIter.Reset(CSSOrderAwareFrameIterator::ChildFilter::IncludeAll);
+  size_t absposIndex = 0;
   const LogicalAxis masonryAxis =
       IsMasonry(LogicalAxis::Block) ? LogicalAxis::Block : LogicalAxis::Inline;
   const auto wm = aGridRI.mWM;
@@ -8813,16 +8814,8 @@ nscoord nsGridContainerFrame::MasonryLayout(GridReflowInput& aGridRI,
       auto* ph = static_cast<nsPlaceholderFrame*>(child);
       auto* oof = ph->GetOutOfFlowFrame();
       if (oof && oof->GetParent() == this) {
-        // mAbsPosItems can include descendants whose placeholders are nested
-        // inside grid items and aren't visited by mIter. Search by frame since
-        // the array indices need not match the order of placeholders we visit.
-        for (auto& absPosItem : aGridRI.mAbsPosItems) {
-          if (absPosItem.mFrame == oof) {
-            item = &absPosItem;
-            break;
-          }
-        }
-        MOZ_RELEASE_ASSERT(item);
+        item = &aGridRI.mAbsPosItems[absposIndex++];
+        MOZ_RELEASE_ASSERT(item->mFrame == oof);
         auto masonryStart = item->mArea.LineRangeForAxis(masonryAxis).mStart;
         // If the item was placed by the author at line 1 (masonryStart == 0)
         // then include it to be placed at the masonry-box start.  If it's
@@ -10096,17 +10089,8 @@ nsFrameState nsGridContainerFrame::ComputeSelfSubgridMasonryBits() const {
   nsFrameState bits = NS_FRAME_STATE_NONE;
   const auto* pos = StylePosition();
 
-  if (StyleDisplay()->DisplayInside() == StyleDisplayInside::GridLanes) {
-    // If rows are defined and columns are none → row tracks;
-    // otherwise (columns defined, both defined, or neither defined) → column
-    // tracks.
-    if (!pos->mGridTemplateRows.IsNone()) {
-      bits |= NS_STATE_GRID_IS_COL_MASONRY;
-    } else {
-      bits |= NS_STATE_GRID_IS_ROW_MASONRY;
-    }
-  } else if (pos->mGridTemplateRows.IsMasonry()) {
-    // We can only have masonry layout in one axis.
+  // We can only have masonry layout in one axis.
+  if (pos->mGridTemplateRows.IsMasonry()) {
     bits |= NS_STATE_GRID_IS_ROW_MASONRY;
   } else if (pos->mGridTemplateColumns.IsMasonry()) {
     bits |= NS_STATE_GRID_IS_COL_MASONRY;
