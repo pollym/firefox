55a689218f99401467d90015a575d4523cf92402
Reem H <42309026+reemhamz@users.noreply.github.com>
2026-09-24 01:22:14 +0000

    Bug 2066246 - Turn POTD widget off by default for US, DE, FR, PL regions. r=maxx
    Differential Revision: https://phabricator.services.mozilla.com/D327245

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2032,6 +2032,12 @@ pref("browser.newtabpage.activity-stream.widgets.focusTimer.region-block", "DE,F
 pref("browser.newtabpage.activity-stream.widgets.system.clocks.region-block", "PL");
 pref("browser.newtabpage.activity-stream.widgets.clocks.region-block", "DE,FR,PL,US");
 
+// Picture of the day is available everywhere the container is except PL, where
+// only the current Nimbus experiment reveals it. Off by default in the US, DE
+// and FR, where the engaged cohort is turned on through Nimbus instead.
+pref("browser.newtabpage.activity-stream.widgets.system.pictureOfTheDay.region-block", "PL");
+pref("browser.newtabpage.activity-stream.widgets.pictureOfTheDay.region-block", "DE,FR,PL,US");
+
 // Promo card visibility
 pref("browser.newtabpage.activity-stream.discoverystream.promoCard.visible", true);
 
