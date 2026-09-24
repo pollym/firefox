5d2eccb08fd2df4f7c5a11234b51de76d028b6e9
Alex Hochheiden <ahochheiden@mozilla.com>
2026-09-24 05:54:58 +0000

    Bug 2071187 - Only set `checkout.workers` for the initial checkout in `bin\bootstrap.py` r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D325583

--- python/mozboot/bin/bootstrap.py
+++ python/mozboot/bin/bootstrap.py
@@ -190,21 +190,6 @@ def hg_clone_firefox(hg: Path, dest: Path, head_repo, head_rev):
     return dest
 
 
-def enable_parallel_checkout(git: Path, dest: Path, env=None):
-    try:
-        subprocess.check_call(
-            [str(git), "config", "--get", "checkout.workers"],
-            cwd=str(dest),
-            env=env,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-    except subprocess.CalledProcessError:
-        subprocess.check_call(
-            [str(git), "config", "checkout.workers", "0"], cwd=str(dest), env=env
-        )
-
-
 def git_clone_firefox(git: Path, dest: Path, head_repo, head_rev):
     if head_repo and "hg.mozilla.org" in head_repo:
         print("GECKO_HEAD_REPOSITORY cannot be a Mercurial repository when using Git")
@@ -220,7 +205,6 @@ def git_clone_firefox(git: Path, dest: Path, head_repo, head_rev):
         ],
     )
     subprocess.check_call([str(git), "config", "pull.ff", "only"], cwd=str(dest))
-    enable_parallel_checkout(git, dest)
 
     if head_repo:
         subprocess.check_call(
@@ -231,6 +215,8 @@ def git_clone_firefox(git: Path, dest: Path, head_repo, head_rev):
     subprocess.check_call(
         [
             str(git),
+            "-c",
+            "checkout.workers=0",
             "checkout",
             "FETCH_HEAD" if head_rev else "main",
             "--",
@@ -314,7 +300,6 @@ def git_cinnabar_clone_firefox(git: Path, dest: Path, head_repo, head_rev):
         subprocess.check_call(
             [str(git), "config", "pull.ff", "only"], cwd=str(dest), env=env
         )
-        enable_parallel_checkout(git, dest, env)
 
         if head_repo:
             subprocess.check_call(
@@ -326,6 +311,8 @@ def git_cinnabar_clone_firefox(git: Path, dest: Path, head_repo, head_rev):
         subprocess.check_call(
             [
                 str(git),
+                "-c",
+                "checkout.workers=0",
                 "checkout",
                 "FETCH_HEAD" if head_rev else "main",
                 "--",
