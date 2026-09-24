cc951cfc8d60e19047a031e983155b2c7cda7b35
Ting-Yu Lin <tlin@mozilla.com>
2026-09-24 15:27:51 +0000

    Bug 1938264 - Don't compute CB block-size again for an inline replaced element with a percentage block-size. r=layout-reviewers,dholbert
    `ComputeContainingBlockRectangle()` already considers `mTreatBSizeAsIndefinite`
    and computes an unconstrained block-size for the containing block when
    `mTreatBSizeAsIndefinite` is true. This happens for a flex item in a column flex
    container with an indefinite block-size.
    
    However, in standards mode, the inline replaced element fell back to the
    containing block's computed block-size, which is a wrong value to use. This
    patch simply deletes the unwanted code.
    
    For the quirks mode behavior, this patch simplifies it while preserving the
    behavior. Also, move `isAutoBSize` closer to its usage that handles internal
    table frames.
    
    Differential Revision: https://phabricator.services.mozilla.com/D328695

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -2156,47 +2156,18 @@ void ReflowInput::InitConstraints(
             StyleLengthPercentage::FromAppUnits(stretchBSize));
       }
     }
-    bool isAutoBSize = blockSize->BehavesLikeInitialValueOnBlockAxis();
 
-    // Check for a percentage based block size and a containing block
-    // block size that depends on the content block size
-    if (blockSize->HasPercent()) {
-      if (NS_UNCONSTRAINEDSIZE == cbSize.BSize(wm)) {
-        // this if clause enables %-blockSize on replaced inline frames,
-        // such as images.  See bug 54119.  The else clause "blockSizeUnit =
-        // eStyleUnit_Auto;" used to be called exclusively.
-        if (mFlags.mIsReplaced && mStyleDisplay->IsInlineOutsideStyle()) {
-          // Get the containing block's reflow input
-          NS_ASSERTION(cbri, "no containing block");
-          // in quirks mode, get the cb height using the special quirk method
-          if (!wm.IsVertical() &&
-              eCompatibility_NavQuirks == aPresContext->CompatibilityMode()) {
-            if (!cbri->mFrame->IsTableCellFrame() &&
-                !cbri->mFrame->IsFlexOrGridItem()) {
-              cbSize.BSize(wm) = CalcQuirkContainingBlockHeight(cbri);
-              if (cbSize.BSize(wm) == NS_UNCONSTRAINEDSIZE) {
-                isAutoBSize = true;
-              }
-            } else {
-              isAutoBSize = true;
-            }
-          }
-          // in standard mode, use the cb block size.  if it's "auto",
-          // as will be the case by default in BODY, use auto block size
-          // as per CSS2 spec.
-          else {
-            nscoord computedBSize = cbri->ComputedSize(wm).BSize(wm);
-            if (NS_UNCONSTRAINEDSIZE != computedBSize) {
-              cbSize.BSize(wm) = computedBSize;
-            } else {
-              isAutoBSize = true;
-            }
-          }
-        } else {
-          // default to interpreting the blockSize like 'auto'
-          isAutoBSize = true;
-        }
-      }
+    // In quirks mode, get the containing block height using the special quirk
+    // method for replaced inline frames, such as images.
+    //
+    // Bug 2075032: Merge the quirks mode logic below into
+    // ComputeContainingBlockRectangle().
+    if (aPresContext->CompatibilityMode() == eCompatibility_NavQuirks &&
+        blockSize->HasPercent() && cbSize.BSize(wm) == NS_UNCONSTRAINEDSIZE &&
+        mFlags.mIsReplaced && mStyleDisplay->IsInlineOutsideStyle() &&
+        !wm.IsVertical() && !cbri->mFrame->IsTableCellFrame() &&
+        !cbri->mFrame->IsFlexOrGridItem()) {
+      cbSize.BSize(wm) = CalcQuirkContainingBlockHeight(cbri);
     }
 
     // Compute our offsets if the element is relatively positioned.  We
@@ -2257,11 +2228,12 @@ void ReflowInput::InitConstraints(
       }
 
       // Calculate the computed block size
-      if (StyleDisplay::TableColumn == mStyleDisplay->mDisplay ||
-          StyleDisplay::TableColumnGroup == mStyleDisplay->mDisplay) {
-        // 'blockSize' property doesn't apply to table columns and column groups
-        isAutoBSize = true;
-      }
+      // 'blockSize' property doesn't apply to table columns and column groups
+      const bool isAutoBSize =
+          StyleDisplay::TableColumn == mStyleDisplay->mDisplay ||
+          StyleDisplay::TableColumnGroup == mStyleDisplay->mDisplay ||
+          nsLayoutUtils::IsAutoBSize(*blockSize, cbSize.BSize(wm));
+
       // calc() with both percentages and lengths acts like 'auto' on internal
       // table elements
       if (isAutoBSize || blockSize->HasLengthAndPercentage()) {
